f2823082dd1262648ffcf756e4f555539d1387de
Maximilian Nickel <nickel@dbs.ifi.lmu.de>
2013-11-09 18:45:43 -0500

    initial checkin

---
+++ examples/kinships.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+
+import logging
+logging.basicConfig(level=logging.INFO)
+_log = logging.getLogger('Example Kinships')
+
+import numpy as np
+from numpy import dot, array, zeros, setdiff1d
+from numpy.linalg import norm
+from numpy.random import shuffle
+from scipy.io.matlab import loadmat
+from scipy.sparse import lil_matrix
+from sklearn.metrics import precision_recall_curve, auc
+from rescal import rescal_als
+
+
+def predict_rescal_als(T):
+    A, R, _, _, _ = rescal_als(
+        T, 100, init='nvecs', conv=1e-3,
+        lambda_A=10, lambda_R=10
+    )
+    n = A.shape[0]
+    P = zeros((n, n, len(R)))
+    for k in range(len(R)):
+        P[:, :, k] = dot(A, dot(R[k], A.T))
+    return P
+
+
+def normalize_predictions(P, e, k):
+    for a in range(e):
+        for b in range(e):
+            nrm = norm(P[a, b, :k])
+            if nrm != 0:
+                # round values for faster computation of AUC-PR
+                P[a, b, :k] = np.round_(P[a, b, :k] / nrm, decimals=3)
+    return P
+
+
+def innerfold(T, mask_idx, target_idx, e, k, sz):
+    Tc = [Ti.copy() for Ti in T]
+    mask_idx = np.unravel_index(mask_idx, (e, e, k))
+    target_idx = np.unravel_index(target_idx, (e, e, k))
+
+    # set values to be predicted to zero
+    for i in range(len(mask_idx[0])):
+        Tc[mask_idx[2][i]][mask_idx[0][i], mask_idx[1][i]] = 0
+
+    # predict unknown values
+    P = predict_rescal_als(Tc)
+    P = normalize_predictions(P, e, k)
+
+    # compute area under precision recall curve
+    prec, recall, _ = precision_recall_curve(GROUND_TRUTH[target_idx], P[target_idx])
+    return auc(recall, prec)
+
+
+if __name__ == '__main__':
+    # load data
+    mat = loadmat('data/alyawarradata.mat')
+    K = array(mat['Rs'], np.float32)
+    e, k = K.shape[0], K.shape[2]
+    SZ = e * e * k
+
+    # copy ground truth before preprocessing
+    GROUND_TRUTH = K.copy()
+
+    # construct array for rescal
+    T = [lil_matrix(K[:, :, i]) for i in range(k)]
+
+    _log.info('Datasize: %d x %d x %d | No. of classes: %d' % (
+        T[0].shape + (len(T),) + (k,))
+    )
+
+    # Do cross-validation
+    FOLDS = 10
+    IDX = range(SZ)
+    shuffle(IDX)
+
+    fsz = int(SZ / FOLDS)
+    offset = 0
+    AUC_train = zeros(FOLDS)
+    AUC_test = zeros(FOLDS)
+    for f in range(FOLDS):
+        idx_test = IDX[offset:offset + fsz]
+        idx_train = setdiff1d(IDX, idx_test)
+        shuffle(idx_train)
+        idx_train = idx_train[:fsz].tolist()
+        _log.info('Train Fold %d' % f)
+        AUC_train[f] = innerfold(T, idx_train + idx_test, idx_train, e, k, SZ)
+        _log.info('Test Fold %d' % f)
+        AUC_test[f] = innerfold(T, idx_test, idx_test, e, k, SZ)
+
+        offset += fsz
+
+    _log.info('AUC-PR Test Mean / Std: %f / %f' % (AUC_test.mean(), AUC_test.std()))
+    _log.info('AUC-PR Train Mean / Std: %f / %f' % (AUC_train.mean(), AUC_train.std()))
---
+++ rescal/__init__.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+# Copyright (C) 2013 Maximilian Nickel <mnick@mit.edu>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+RESCAL Python package
+
+This package provides routines to compute various forms of
+the RESCAL tensor factorization.
+
+RESCAL factors a (usually sparse) three-way tensor $\ten{X}$ such that each
+frontal slice $X_k$ is factored into
+
+.. math:: X_k = A * R_k * A.T.
+
+The frontal slices of a tensor are $N \times N$ matrices. Usually, these
+matrices correspond to the sparse adjacency matrices of the relational graph
+for a particular relation in a multi-relational data set.
+
+See
+---
+For a full description of the algorithm see:
+.. [1] Maximilian Nickel, Volker Tresp, Hans-Peter-Kriegel,
+       "A Three-Way Model for Collective Learning on Multi-Relational Data",
+       ICML 2011, Bellevue, WA, USA
+
+.. [2] Maximilian Nickel, Volker Tresp, Hans-Peter-Kriegel,
+       "Factorizing YAGO: Scalable Machine Learning for Linked Data"
+       WWW 2012, Lyon, France
+"""
+
+from rescal import als as rescal_als
